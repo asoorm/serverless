1a2a37195a4d7d81b1cda134cc92060fec761b0c
Ahmet Soormally <ahmet@mangomm.co.uk>
2018-08-08 23:34:23 +0100

    fixing azure configs

--- provider/azure/azure.go
+++ provider/azure/azure.go
@@ -5,17 +5,38 @@ import (
 	"github.com/kataras/go-errors"
 )
 
-func init() {
-	provider.RegisterProvider("azure-functions", NewAzureProvider)
+const (
+	//azureUrlFormat = "https://tyk-hello.azurewebsites.net/api/HttpTriggerJS1?code=mZT2aajwLKmWb64URhNgJr5LuCyVah66loou8nDBJj4qF8B30FlMrg=="
+	azureUrlFormat = "https://%s.azurewebsites.net/api/%s?code=%s"
+)
+
+type Conf struct {
+	AppName      string
+	FunctionName string
+	AuthCode     string
 }
 
-func NewAzureProvider() (provider.Provider, error) {
+func init() {
+	provider.RegisterProvider("azure-functions", NewProvider)
+}
+
+func NewProvider() (provider.Provider, error) {
 	return &Provider{}, nil
 }
 
-type Provider struct{}
+type Provider struct {
+	Conf
+}
 
 func (p *Provider) Init(conf provider.Conf) error {
+
+	c, ok := conf.(Conf)
+	if !ok {
+		return errors.New("unable to resolve conf type")
+	}
+	p.AppName = c.AppName
+	p.AuthCode = c.AuthCode
+	p.FunctionName = c.FunctionName
 
 	return nil
 }
@@ -25,7 +46,7 @@ func (Provider) List() ([]provider.Function, error) {
 	return nil, errors.New("azure doesnt allow listing functions")
 }
 
+// TODO
 func (p Provider) Invoke(function provider.Function, requestBody []byte) (*provider.Response, error) {
-
 	return nil, nil
 }
